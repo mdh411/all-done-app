d056d6a5a9ede78070085f25421624b35f46738a
Hussain, Mohammed D <dilawarhussain1511@gmail.com>
2024-07-09 14:57:18 +0100

    Feature: Basic To Do UI
    Created a basic UI to add and delete tasks

--- app.py
+++ app.py
@@ -1,23 +1,49 @@
-from flask import Flask, jsonify, request, render_template
+import random
+from flask import Flask, jsonify, redirect, request, render_template, url_for
 
 app = Flask(__name__)
 
 tasks = [
-    {"id": 1, "title": "Task 1", "description": "This is Task 1", "status": False},
-    {"id": 2, "title": "Task 2", "description": "This is Task 2", "status": False},
+    {"id": 1, "name": "Pray Asr", "checked": False},
+    {"id": 2, "name": "Pray Maghrib", "checked": True},
 ]
 
 @app.route("/")
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if (request.method == "POST"):
+        task_name = request.form["task_name"]
+        cur_id = random.randint(1, 1000)
+        tasks.append({
+            'id': cur_id,
+            'name': task_name,
+            'checked': False
+        })
+    return render_template("index.html", items=tasks)
+
+@app.route("/checked/<int:task_id>", methods=["POST"])
+def checked_task(task_id):
+    for task in tasks:
+        if task['id'] == task_id:
+            task['checked'] = not task['checked']
+            break
+    return redirect(url_for("home"))
+
+
+@app.route("/delete/<int:task_id>", methods=["POST"])
+def delete_task(task_id):
+    global tasks
+    for task in tasks:
+        if task['id'] == task_id:
+            tasks.remove(task)
+    return redirect(url_for("home"))
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
     new_task = {
         "id": len(tasks) + 1,
-        "title": request.json['title'],
-        "description": request.json.get('description', 'This is Task 3'),
-        "status": False
+        "name": request.json['name'],
+        "checked": False
     }
     tasks.append(new_task)
     return jsonify({"message": "Task created successfully", "task": new_task}), 201
@@ -32,17 +58,16 @@ def edit_tasks(task_id):
     if not task:
         return jsonify({"message": "Task not found"}), 404
 
-    task['title'] = request.json.get('title', task['title'])
-    task['description'] = request.json.get('description', task['description'])
-    task['status'] = request.json.get('status', task['status'])
+    task['name'] = request.json.get('name', task['name'])
+    task['checked'] = request.json.get('checked', task['checked'])
     
     return jsonify({"message": "Task updated successfully", "task": task}), 200
 
-@app.route('/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    global tasks
-    tasks = [task for task in tasks if task['id'] != task_id]
-    return jsonify({"message": "Task deleted successfully"}), 200
+# @app.route('/tasks/<int:task_id>', methods=['DELETE'])
+# def delete_task(task_id):
+#     global tasks
+#     tasks = [task for task in tasks if task['id'] != task_id]
+#     return jsonify({"message": "Task deleted successfully"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
