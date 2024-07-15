53b6cad8e049a79e5356a788365c1dd0e04c3685
Hussain, Mohammed D <dilawarhussain1511@gmail.com>
2024-07-15 01:33:36 +0100

    Tests for App.js
    Parent (App.js) file tested only for now as other components likely to change significantly in coming tickets.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the create task form', () => {
+  const { getByPlaceholderText } = render(<App />);
+  const inputElement = getByPlaceholderText(/New task/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('allows users to add a task', async () => {
+  const { getByPlaceholderText, getByText, getByRole } = render(<App />);
+  const inputElement = getByPlaceholderText(/New task/i);
+  const buttonElement = getByRole('button', { name: /Add Task/i });
+
+  // simulate user click
+  fireEvent.change(inputElement, { target: { value: 'New Task' } });
+  fireEvent.click(buttonElement);
+
+  // wait for the page to update
+  await waitFor(() => getByText('New Task'));
+
+  // check task was added
+  expect(getByText('New Task')).toBeInTheDocument();
 });
