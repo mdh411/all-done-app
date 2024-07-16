099bc06d4309e3b00413d166d2320a728114b736
Hussain, Mohammed D <dilawarhussain1511@gmail.com>
2024-07-16 20:11:05 +0100

    Input validation for modals
    Also refactored test files for modals - created helper functions and reusable consts. Including paramaterised tests.

--- frontend/src/components/Tasks/EditTaskModal.js
+++ frontend/src/components/Tasks/EditTaskModal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
-import './EditTaskModal.css';
+import './AddTaskModal.css';
 
 Modal.setAppElement('#root');
 
@@ -24,6 +24,7 @@ const EditTaskModal = ({ isOpen, onRequestClose, onEditTask, task }) => {
       return;
     }
     onEditTask(task.id, taskName);
+    setError('');
     onRequestClose();
   };
 
@@ -35,18 +36,18 @@ const EditTaskModal = ({ isOpen, onRequestClose, onEditTask, task }) => {
       className="modal"
       overlayClassName="overlay"
     >
-      <button className="close-button" onClick={onRequestClose}>&times;</button>
       <h2>Edit Task</h2>
-      {error && <div data-testid="error-message" className="error-message">{error}</div>}
+      {error && <div className="error-message" data-testid="error-message">{error}</div>}
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter Updated Task"
           value={taskName}
           onChange={(e) => setTaskName(e.target.value)}
+          data-testid="task-input"
         />
         <button type="submit" data-testid="modal-edit-task-button">Edit Task</button>
-        <button type="button" onClick={onRequestClose} data-testid="modal-cancel-button">Cancel</button>
+        <button type="button" onClick={onRequestClose} data-testid="edit-modal-cancel-button">Cancel</button>
       </form>
     </Modal>
   );
